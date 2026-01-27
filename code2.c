1ba53905c33f1cbb7e94a549e58b99c7c43883f3
vinithmeenakshi-ctrl <vinithmeenakshi@gmail.com>
2026-01-27 11:11:01 +0530

    Add files via upload

---
+++ code2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    printf("%lu\n", sizeof(int));
+    printf("%lu\n", sizeof(float));
+    printf("%lu\n", sizeof(double));
+    printf("%lu", sizeof(char));
+    return 0;
+}
