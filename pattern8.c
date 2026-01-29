ce7b47319c3f7f08e7d6ec8625a58975248a6312
vinithmeenakshi-ctrl <vinithmeenakshi@gmail.com>
2026-01-29 17:12:42 +0530

    Add files via upload

---
+++ pattern8.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    for (int i = 5; i >= 1; i--) {
+        for (int k = 1; k <= 5 - i; k++) {
+            printf(" ");
+        }
+        for (int j = 1; j < 2 * i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+
+
+
+
