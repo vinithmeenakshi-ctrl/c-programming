991a33829a65e213315ce555e507e26a02833061
vinithmeenakshi-ctrl <vinithmeenakshi@gmail.com>
2026-01-30 16:40:26 +0530

    Add files via upload

---
+++ pattern17.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    for(int i=1;i<5; i++){
+       for(int k=0;k<5-i; k++){
+        printf(" ");
+
+       }
+       for(int j=0; j<2*i;j++){
+        printf("%c",'A'+);
+       }
+       printf("\n");
+
+    }
+    return 0;
+}
