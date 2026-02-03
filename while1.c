7f50452e55a72f0ee746b58ca457526301b8d7f4
vinithmeenakshi-ctrl <vinithmeenakshi@gmail.com>
2026-02-03 15:42:29 +0530

    Add files via upload

--- while1.c
+++ while1.c
@@ -1,4 +1,32 @@
-#include<stdio.h>
-int main(){
-    
+#include <stdio.h>
+
+int main() {
+    int n, noise;
+    int i = 0;
+    int violations = 0;
+    int streak = 0;
+    int maxStreak = 0;
+
+    scanf("%d", &n);
+
+    while (i < n) {
+        scanf("%d", &noise);
+
+        if (noise > 70) {
+            violations++;     
+            streak++;        
+            if (streak > maxStreak) {
+                maxStreak = streak;
+            }
+        } else {
+            streak = 0;       
+        }
+
+        i++;
+    }
+
+    printf("Noise Violations: %d\n", violations);
+    printf("Longest Violation Streak: %d\n", maxStreak);
+
+    return 0;
 }
