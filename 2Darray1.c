6c42c12fa9a5b1ec59b2e75214ae622333e9f6c4
vinithmeenakshi-ctrl <vinithmeenakshi@gmail.com>
2026-02-06 20:54:45 +0530

    Add files via upload

---
+++ 2Darray1.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main(){
+    int r,c;
+    scanf("%d %d",&r,&c);
+
+    int arr[r][c];
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            scanf("%d",&arr[i][j]);
+        }
+    }
+     int max=0;
+         int second = 0;
+    
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            if(arr[i][j] > max){
+                max = arr[i][j];
+            }
+        
+            else if(arr[i][j] > second){
+                second = arr[i][j];
+            }
+               }
+        
+}
+ printf("%d ",second);
+         return 0;
+}
