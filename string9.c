c5aa9d2146f02338945cae9339f39caaced02ccf
vinithmeenakshi-ctrl <vinithmeenakshi@gmail.com>
2026-02-11 10:47:57 +0530

    Add files via upload

---
+++ string9.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    char str[100];
+    fgets(str,sizeof(str),stdin);
+
+
+    for(int i=0;str[i]!='\0';i++){
+        if(str[i]>='a' && str[i]<='z'){
+            str[i]=str[i]-32;
+        }
+    }
+    printf("%s",str);
+    return 0;
+
+
+
+
+}
